7a6af5254a95feec577a72d4cc6f0d04b140eefe
saropi99 <saropi9978@gmail.com>
2021-02-28 19:45:25 -0500

    actividad de gestionar ruta completada

--- app/src/main/java/com/example/truckers/view/ui/MapsActivity.java
+++ app/src/main/java/com/example/truckers/view/ui/MapsActivity.java
@@ -1,12 +1,16 @@
-package com.example.truckers;
+package com.example.truckers.view.ui;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageButton;
 
 import androidx.appcompat.widget.Toolbar;
+
+import com.example.truckers.R;
 
 public class MapsActivity extends AppCompatActivity {
 
@@ -14,8 +18,15 @@ public class MapsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        ImageButton backBtn = (ImageButton) findViewById(R.id.home_back_button);
         getSupportActionBar().setTitle("Defina su ruta");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
     }
 }
