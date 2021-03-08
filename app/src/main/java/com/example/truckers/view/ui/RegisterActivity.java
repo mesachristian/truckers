912c294f18f3cfa6711e1e465b55e689306dd453
Christian Mesa <mesachristian@hotmail.com>
2021-03-08 10:51:33 -0500

    Register

--- app/src/main/java/com/example/truckers/view/ui/RegisterActivity.java
+++ app/src/main/java/com/example/truckers/view/ui/RegisterActivity.java
@@ -1,21 +1,60 @@
 package com.example.truckers.view.ui;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.truckers.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.IOException;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private static final int PICK_IMAGE = 1;
+    private static final int TAKE_IMAGE = 2;
+
+    private ImageView profilePicture;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        // Camera permission
+        if(ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(RegisterActivity.this,
+                    new String[]{
+                            Manifest.permission.CAMERA
+                    }, TAKE_IMAGE);
+        }
+
+        profilePicture = (ImageView) findViewById(R.id.register_profile_pic);
         ImageButton backBtn = (ImageButton) findViewById(R.id.register_back_button);
 
         backBtn.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +64,71 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+        ImageButton galleryBtn = (ImageButton) findViewById(R.id.select_media_btn);
+        galleryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent();
+                galleryIntent.setAction(android.content.Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
 
+                startActivityForResult(Intent.createChooser(galleryIntent,"Seleccione una imagen"), PICK_IMAGE);
+            }
+        });
+
+        ImageButton cameraBtn = (ImageButton) findViewById(R.id.use_camera_btn);
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, TAKE_IMAGE);
+            }
+        });
+
+        Button registerBtn = (Button) findViewById(R.id.register_btn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void createUserInFirebase(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PICK_IMAGE && resultCode == RESULT_OK){
+            Uri imageUri = data.getData();
+            try{
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                profilePicture.setImageBitmap(bitmap);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+        }
+
+        if(requestCode == TAKE_IMAGE && resultCode == RESULT_OK){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            profilePicture.setImageBitmap(bitmap);
+        }
     }
 }
