453f8b2127f59df494b4bff726879ad40e73df6e
saropi99 <saropi9978@gmail.com>
2021-04-12 23:14:34 -0500

    creaciónde mapa con posicion

--- app/src/main/java/com/example/truckers/view/ui/StartRouteMapActivity.java
+++ app/src/main/java/com/example/truckers/view/ui/StartRouteMapActivity.java
@@ -2,15 +2,26 @@ package com.example.truckers.view.ui;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.hardware.SensorManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.truckers.R;
+
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,15 +29,35 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
+import java.io.IOException;
+
 public class StartRouteMapActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    FusedLocationProviderClient client;
-    SupportMapFragment mapFragment;
     private GoogleMap mMap;
+    private Marker locationM;
+    private Marker searchM;
+    private FusedLocationProviderClient clientLocation;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+
+    //Permisos
+    String permLocation = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final int LOCATION_PERMISSION_ID = 15;
+
+    //Settings
+    private static final int SETTINGS_GPS = 20;
+
+    //views
+    EditText address;
+    //Geocoder
+    Geocoder geocoder;
+    SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,63 +66,122 @@ public class StartRouteMapActivity extends FragmentActivity implements OnMapRead
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        client = LocationServices.getFusedLocationProviderClient(this);
+        System.out.println("antes");
+        clientLocation = LocationServices.getFusedLocationProviderClient(this);
+        locationRequest = createLocationRequest();
+        System.out.println("despues");
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                Location location = locationResult.getLastLocation();
+                if (location != null) {
+                    Log.i(" LOCATION ", "Longitud: " + location.getLongitude());
+                    Log.i(" LOCATION ", "Latitud: " + location.getLatitude());
 
-        /*/permisos
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            getCurrentLocation();
-        }else{
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
-        }*/
+                    System.out.println("*******************************");
+                    System.out.println(location.getLatitude());
 
-    }
-/*
-    private void getCurrentLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Task<Location> task = client.getLastLocation();
-            task.addOnSuccessListener(new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(final Location location) {
-                    if (location != null) {
-                        mapFragment.getMapAsync(new OnMapReadyCallback() {
-                            @Override
-                            public void onMapReady(GoogleMap googleMap) {
-                                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                                MarkerOptions options = new MarkerOptions().position(latLng).title("estoy aqui");
-
-                                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-                                googleMap.addMarker(options);
-
-                            }
-                        });
-                    }
+                    LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    locationM = mMap.addMarker(new MarkerOptions().position(userLocation).title("Tu Ubicación"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+                    mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
 
                 }
-            });
+
+            }
+
+        };
+
+        int permissionLocation1 = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        int permissionLocation2 = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if (permissionLocation1 != PackageManager.PERMISSION_GRANTED
+                && permissionLocation2 != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1000);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 2000);
+        }
+    }
+
+    private void startLocationUpdates() {
+        //Verificación de permiso!!
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            clientLocation.requestLocationUpdates(locationRequest, locationCallback, null);
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode==44){
-            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                getCurrentLocation();
-            }
-        }
+    protected void onResume() {
+        super.onResume();
+        startLocationUpdates();
     }
-*/
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+    private void stopLocationUpdates(){
+        clientLocation.removeLocationUpdates(locationCallback);
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        LatLng bogota = new LatLng(4.65, -74.05);
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(10));
         mMap.getUiSettings().setZoomGesturesEnabled(true);
         mMap.getUiSettings().setZoomControlsEnabled(true);
-        mMap.addMarker(new MarkerOptions().position(bogota).
-                title("Marker in Bogotá")
-                .alpha(0.5f)
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(bogota));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                if(searchM != null)
+                {
+                    searchM.remove();
+                }
+                searchM=mMap.addMarker(new MarkerOptions().position(latLng).title(geoCoderSearchLatLang(latLng)).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+                Double latitude1 = searchM.getPosition().latitude;
+                Double latitude2 = locationM.getPosition().latitude;
+                Double longitude1 = searchM.getPosition().longitude;
+                Double longitude2 = locationM.getPosition().longitude;
+                Toast.makeText(StartRouteMapActivity.this, "La distancia es: " + distance(latitude1, longitude1, latitude2, longitude2) + " Km", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    protected LocationRequest createLocationRequest() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000); //tasa de refresco en milisegundos
+        locationRequest.setFastestInterval(5000); //máxima tasa de refresco
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        return locationRequest;
+    }
+
+    private String geoCoderSearchLatLang(LatLng latLng) {
+        String finalName="";
+        try {
+            Address name = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1).get(0);
+            finalName = name.getAddressLine(0);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return finalName;
+    }
+
+    public double distance(double lat1, double long1, double lat2, double long2) {
+        double latDistance = Math.toRadians(lat1 - lat2);
+        double lngDistance = Math.toRadians(long1 - long2);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lngDistance / 2) * Math.sin(lngDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double result = 6371 * c;
+        return Math.round(result*100.0)/100.0;
     }
 }
