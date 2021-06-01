738c17469309ed43345878e912a76563c5296096
Geraldine <avocadoenterprise@gmail.com>
2021-05-31 19:01:35 -0500

    Alarmas

--- app/src/main/java/com/example/truckers/AlarmActivity.java
+++ app/src/main/java/com/example/truckers/AlarmActivity.java
@@ -8,8 +8,11 @@ import androidx.lifecycle.Lifecycle;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.EventLog;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.example.truckers.view.ui.MapsActivity;
 
@@ -21,6 +24,20 @@ public class AlarmActivity extends AppCompatActivity {
         setContentView(R.layout.activity_alarm);
         ImageButton backBtn = (ImageButton) findViewById(R.id.home_back_button);
         getSupportActionBar().setTitle("Mis Alarmas");
+
+        TextView text2 = (TextView) findViewById(R.id.editTextTextPersonName4);
+        Switch sw2 = (Switch) findViewById(R.id.switch3);
+        Log.println(Log.ASSERT,"AA",String.valueOf(text2.getVisibility()));
+        if(text2.getVisibility() == 0) {
+            Log.println(Log.ASSERT,"AA","AAAA");
+            sw2.setVisibility(View.VISIBLE);
+            text2.setVisibility(View.VISIBLE);
+        }
+
+        TextView text = (TextView) findViewById(R.id.editTextTextPersonName4);
+        Switch sw = (Switch) findViewById(R.id.switch3);
+        sw.setVisibility(View.INVISIBLE);
+        text.setVisibility(View.INVISIBLE);
 
     }
 
--- app/src/main/java/com/example/truckers/CustomAlarmActivity.java
+++ app/src/main/java/com/example/truckers/CustomAlarmActivity.java
@@ -2,7 +2,15 @@ package com.example.truckers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.example.truckers.model.Alarm;
+import com.example.truckers.view.ui.MapsActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CustomAlarmActivity extends AppCompatActivity {
 
@@ -12,4 +20,12 @@ public class CustomAlarmActivity extends AppCompatActivity {
         setContentView(R.layout.activity_custom_alarm);
         getSupportActionBar().setTitle("Nueva Alarma");
     }
+
+    public void addAlarm(View v){
+
+        startActivity(new Intent(this, AlarmActivity.class));
+    }
+
+
+
 }
